7f40f57323ade843851a43b5e7173690a39ab8df
Marcelo Azevedo <marceloazvedo1@gmail.com>
2020-07-21 16:36:39 -0300

    *init-project*

---
+++ src/main/java/br/com/indra/marceloazevedo/SelecaoJava/SelecaoJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.indra.marceloazevedo.SelecaoJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelecaoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelecaoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/indra/marceloazevedo/SelecaoJava/SelecaoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.indra.marceloazevedo.SelecaoJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelecaoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
